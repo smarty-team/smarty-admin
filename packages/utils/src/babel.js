0303b2c0b3a0004ffc85c94b41b138a998b34c30
josephxia <15906475@qq.com>
2022-03-18 15:44:11 +0800

    update babel

---
+++ packages/utils/src/babel.js
@@ -0,0 +1,12 @@
+import "@babel/polyfill"
+const p = new Promise((resolve, reject) => {
+  resolve("wens");
+});
+
+// const关键字
+const a = '123'
+// 箭头语法
+const add = (a,b) => a + b
+// Set
+const set = new Set([1,2,3])
+
