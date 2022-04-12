8c459946ff1ae5d4540f1b7f7bd141b4e0a3bfa9
josephxia <15906475@qq.com>
2022-04-12 11:54:46 +0800

    chore: add husky

--- packages/ui/scripts/verify-commit.js
+++ packages/ui/scripts/verify-commit.js
@@ -2,7 +2,7 @@ const msgPath = process.env.HUSKY_GIT_PARAMS
 const msg = require('fs')
 .readFileSync(msgPath, 'utf-8')
 .trim()
-
+console.log('===============')
 const commitRE = /^(feat|fix|docs|style|refactor|perf|test|workflow|build|ci|chore|release|workflow)(\(.+\))?: .{1,50}/
 
 if (!commitRE.test(msg)) {
