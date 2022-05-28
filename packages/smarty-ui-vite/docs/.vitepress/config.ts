6cb036cf7bb3c98b0133252890328674caca6abf
josephxia <15906475@qq.com>
2022-05-28 20:59:34 +0800

    feat: add vitepress docs

---
+++ packages/smarty-ui-vite/docs/.vitepress/config.ts
@@ -0,0 +1,33 @@
+const sidebar = {
+  '/': [
+    { text: '快速开始', link: '/' },
+    {
+      text: '通用',
+      children: [
+        { text: 'Button 按钮', link: '../components/button/' },
+
+      ]
+    },
+    { text: '导航' },
+    { text: '反馈' },
+    { text: '数据录入' },
+    { text: '数据展示' },
+    { text: '布局' }
+  ]
+}
+const config = {
+  themeConfig: {
+    sidebar
+  },
+  markdown: {
+    config: (md) => {
+      // 这里可以使用 markdown-it 插件，vitepress-theme-demoblock就是基于此开发的
+      const { demoBlockPlugin } = require('vitepress-theme-demoblock')
+      md.use(demoBlockPlugin)
+    }
+  }
+}
+
+export default config
+
+
