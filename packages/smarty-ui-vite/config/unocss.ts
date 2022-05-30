8a5cd983257e0cc2dab7c8dc48d0aa4ce5c04398
josephxia <15906475@qq.com>
2022-05-30 10:24:26 +0800

    docs: add Readme

---
+++ packages/smarty-ui-vite/config/unocss.ts
@@ -0,0 +1,41 @@
+import { presetUno, presetAttributify, presetIcons } from "unocss";
+import Unocss from "unocss/vite";
+
+const colors = [
+  "white",
+  "black",
+  "gray",
+  "red",
+  "yellow",
+  "green",
+  "blue",
+  "indigo",
+  "purple",
+  "pink",
+];
+const safelist = [
+  ...colors.map((v) => `bg-${v}-100`),
+  ...colors.map((v) => `bg-${v}-400`),
+  ...colors.map((v) => `bg-${v}-500`),
+  ...colors.map((v) => `hover:bg-${v}-100`),
+  ...colors.map((v) => `hover:bg-${v}-300`),
+  ...colors.map((v) => `hover:bg-${v}-400`),
+  ...colors.map((v) => `border-${v}-400`),
+  ...colors.map((v) => `border-${v}-500`),
+  ...colors.map((v) => `text-${v}`),
+  ...colors.map((v) => `hover:text-${v}`),
+
+  ...Array.from({ length: 8 }, (_, i) => `px-${i + 1}`),
+  ...Array.from({ length: 8 }, (_, i) => `py-${i + 1}`),
+  ...["xs", "sm", "base", "lg", "xl", "2xl", "3xl"].map((v) => `text-${v}`),
+  ...["rounded-full", "rounded-lg"],
+  ...["search", "edit", "check", "message", "star-off", "delete", "add"].map(
+    (v) => `i-ic-baseline-${v}`
+  ),
+];
+
+export default () =>
+  Unocss({
+    safelist,
+    presets: [presetUno(), presetAttributify(), presetIcons()],
+  });
