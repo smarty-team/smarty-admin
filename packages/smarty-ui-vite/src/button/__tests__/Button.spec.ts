d0dc5deda9ae8e998284426507c93efa714fb20d
josephxia <15906475@qq.com>
2022-06-18 21:00:05 +0800

    feat: add button test case

--- packages/smarty-ui-vite/src/button/__tests__/Button.spec.ts
+++ packages/smarty-ui-vite/src/button/__tests__/Button.spec.ts
@@ -1,23 +1,125 @@
 import Button from "../Button";
-// import { shallowMount } from "@testing-library/vue";
 
 import { shallowMount } from "@vue/test-utils";
-import { expect, test } from "vitest";
-test("it should work", () => {
-  // const { getByText } = render({
-  //   template: `<SButton>Button</SButton>`,
-  //   components:{
-  //       'SButton' :Button
-  //   }
-  // });
+import { getByAltText, render } from "@testing-library/vue";
+import { describe, expect, test } from "vitest";
 
-  // // assert output
-  // getByText("Button");
+describe('Button', () => {
+  test("mount  @vue/test-utils", () => {
+    // @vue/test-utils
+    const wrapper = shallowMount(Button, {
+      slots: {
+        default: 'Button'
+      }
+    });
+    expect(wrapper.text()).toBe("Button");
 
-  const wrapper = shallowMount(Button, {
-    slots: {
-      default: 'Button'
-    }
   });
-  expect(wrapper.text()).toBe("Button");
-});
+
+  test("mount  @testing-library/vue", () => {
+    const { getByText } = render(Button, {
+      slots: {
+        default: 'Button'
+      }
+    });
+    // // assert output
+    getByText("Button");
+  });
+
+  describe('color', () => {
+    test("default", () => {
+      const wrapper = shallowMount(Button, {
+        slots: {
+          default: 'Button'
+        }
+      });
+
+      expect(wrapper.classes().includes('bg-blue-500')).toBe(true)
+    });
+
+
+    test("red", () => {
+      const wrapper = shallowMount(Button, {
+        slots: {
+          default: 'Button'
+        },
+        props: {
+          color: 'red'
+        }
+      });
+
+      expect(wrapper.classes().includes('bg-red-500')).toBe(true)
+    });
+  })
+
+
+  describe('round', () => {
+    test("default", () => {
+      const wrapper = shallowMount(Button, {
+        slots: {
+          default: 'Button'
+        }
+      });
+
+      expect(wrapper.classes().includes('rounded-lg')).toBe(true)
+    });
+
+
+    test("round:true", () => {
+      const wrapper = shallowMount(Button, {
+        slots: {
+          default: 'Button'
+        },
+        props: {
+          round: true
+        }
+      });
+
+      expect(wrapper.classes().includes('rounded-full')).toBe(true)
+    });
+  })
+
+
+
+  describe('plain', () => {
+    test("default", () => {
+      const wrapper = shallowMount(Button, {
+        slots: {
+          default: 'Button'
+        }
+      });
+
+      expect(wrapper.classes().includes('bg-blue-500')).toBe(true)
+    });
+
+
+    test("plain:true", () => {
+      const wrapper = shallowMount(Button, {
+        slots: {
+          default: 'Button'
+        },
+        props: {
+          plain: true
+        }
+      });
+
+      expect(wrapper.classes().includes('bg-blue-100')).toBe(true)
+    });
+  })
+
+  test("icon", () => {
+    const wrapper = shallowMount(Button, {
+      slots: {
+        default: 'Button'
+      },
+      props: {
+        icon: 'edit'
+      }
+    });
+
+    expect(wrapper.find('i').classes().includes('i-ic-baseline-edit')).toBe(true)
+  });
+})
+
+
+
