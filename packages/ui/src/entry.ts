28ab21f48bb7a998431e234d4e40abd51b26bd59
josephxia <15906475@qq.com>
2022-07-06 16:05:11 +0800

    feat: docs: add

--- packages/ui/src/entry.ts
+++ packages/ui/src/entry.ts
@@ -1,12 +1,14 @@
 import MyButtonOrigin from "./MyButtonOrigin";
 import SFCButton from "./SFCButton.vue";
 import JSXButton from "./JSXButton";
+import MyButton from "./my-button";
 import { App } from "vue";
 export { MyButtonOrigin };
 
 export default {
   install(app: App) {
-    app.component(MyButtonOrigin.name, MyButtonOrigin);
+    // app.component(MyButtonOrigin.name, MyButtonOrigin);
+    app.component(MyButton.name, MyButton)
     app.component(JSXButton.name, JSXButton);
     app.component(SFCButton.name, SFCButton);
   },
--- packages/ui/src/my-button/MyButton.vue
+++ packages/ui/src/my-button/MyButton.vue
@@ -1,8 +1,27 @@
 <template>
-  <button>MyButton</button>
+  <button type="button">
+    <slot></slot>
+  </button>
 </template>
 <script>
+// import { ref } from "vue";
 export default {
+  // class="
+  //   py-2 px-4
+  //   bg-indigo-600
+  //   hover:bg-indigo-700
+  //   focus:ring-indigo-500
+  //   focus:ring-offset-indigo-200
+  //   text-white
+  //   w-full
+  //   transition ease-in duration-200
+  //   text-center text-base font-semibold
+  //   shadow-md
+  //   focus:outline-none
+  //   focus:ring-2
+  //   focus:ring-offset-2
+  //   rounded-lg
+  //   ">
   name: "MyButton",
 };
 </script>
