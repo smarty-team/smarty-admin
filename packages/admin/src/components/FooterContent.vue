f6caa68aedcc61bb2988c608ce4104a5e599c133
josephxia <15906475@qq.com>
2022-08-03 20:19:03 +0800

    feat: fix footer bug

--- packages/admin/src/components/FooterContent.vue
+++ packages/admin/src/components/FooterContent.vue
@@ -1,18 +1,10 @@
 <template>
-  <footer class="bg-white py-2 px-6 dark:bg-gray-900/70">
+  <footer class="bg-white py-2 px-6 dark:bg-gray-900/70 fixed inset-x-0 bottom-0">
     <div class="justify-between items-center block">
-      <div
-        class="flex shrink-0 grow-0 items-center justify-center mb-6 md:mb-0"
-      >
+      <div class="flex shrink-0 grow-0 items-center justify-center mb-6 md:mb-0">
         <div class="text-center md:text-left">
-          <b
-            >© 2022,
-            <a
-              href="https://github.com/smarty-team/smarty-admin"
-              target="_blank"
-              >smarty-admin</a
-            ></b
-          >
+          <b>© 2022,
+            <a href="https://github.com/smarty-team/smarty-admin" target="_blank">smarty-admin</a></b>
         </div>
       </div>
     </div>
