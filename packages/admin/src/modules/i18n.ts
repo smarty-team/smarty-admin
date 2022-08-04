24562ea72c955e1d5388d09fc1cdae1666204e73
josephxia <15906475@qq.com>
2022-08-04 16:23:14 +0800

    fix: fix i18n bug

--- packages/admin/src/modules/i18n.ts
+++ packages/admin/src/modules/i18n.ts
@@ -1,18 +1,17 @@
 import { createI18n } from "vue-i18n";
 
-import en from "~/locales/en.json";
-import zh_CN from "~/locales/zh-CN.json";
+// import en from "~/locales/en.json";
+// import zh_CN from "~/locales/zh-CN.json";
 
 import { UserModule } from "./types";
-
+import messages from '@intlify/vite-plugin-vue-i18n/messages'
 export const install: UserModule = ({ app }) => {
+
+  console.log("message", messages)
   const i18n = createI18n({
     legacy: false,
     locale: "en",
-    messages: {
-      en,
-      zh_CN
-    },
+    messages
   });
 
   app.use(i18n);
