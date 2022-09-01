b68de7a0af63ab5a15c8ed662e4baad97648abdb
wangxiang <wangxiang@quickshopx.com>
2022-09-01 11:58:24 +0800

    fix: build script 'outDir' problem & async problem

--- packages/smarty-ui-vite/scripts/build.ts
+++ packages/smarty-ui-vite/scripts/build.ts
@@ -10,54 +10,55 @@ const buildAll = async () => {
   await build(defineConfig(config as UserConfig) as InlineConfig);
   // await build(defineConfig({}))
 
+  const baseOutDir = config.build.outDir;
   // 复制 Package.json 文件
   const packageJson = require("../package.json");
   packageJson.main = "smarty-ui.umd.js";
   packageJson.module = "smarty-ui.esm.js";
   fs.outputFile(
-    path.resolve(config.build.outDir, `package.json`),
+    path.resolve(baseOutDir, `package.json`),
     JSON.stringify(packageJson, null, 2)
   );
 
   // 拷贝 README.md文件
   fs.copyFileSync(
     path.resolve("./README.md"),
-    path.resolve(config.build.outDir + "/README.md")
+    path.resolve(baseOutDir + "/README.md")
   );
 
   const srcDir = path.resolve(__dirname, "../src/");
-  fs.readdirSync(srcDir)
-    .filter((name) => {
-      // 只要目录不要文件，且里面包含index.ts
-      const componentDir = path.resolve(srcDir, name);
-      const isDir = fs.lstatSync(componentDir).isDirectory();
-      return isDir && fs.readdirSync(componentDir).includes("index.ts");
-    })
-    .forEach(async (name) => {
-      const outDir = path.resolve(config.build.outDir, name);
-      const custom = {
-        lib: {
-          entry: path.resolve(srcDir, name),
-          name, // 导出模块名
-          fileName: `index`,
-          formats: [`es`, `umd`],
-        },
-        outDir,
-      };
+  const componentsDir = fs.readdirSync(srcDir).filter((name) => {
+    // 只要目录不要文件，且里面包含index.ts
+    const componentDir = path.resolve(srcDir, name);
+    const isDir = fs.lstatSync(componentDir).isDirectory();
+    return isDir && fs.readdirSync(componentDir).includes("index.ts");
+  });
+  // forEach中异步执行有问题 改为for-of
+  for (let name of componentsDir) {
+    const outDir = path.resolve(baseOutDir, name);
+    const custom = {
+      lib: {
+        entry: path.resolve(srcDir, name),
+        name, // 导出模块名
+        fileName: `index`,
+        formats: [`es`, `umd`],
+      },
+      outDir,
+    };
 
-      Object.assign(config.build, custom);
-      await build(defineConfig(config as UserConfig) as InlineConfig);
+    Object.assign(config.build, custom);
+    await build(defineConfig(config as UserConfig) as InlineConfig);
 
-      fs.outputFile(
-        path.resolve(outDir, `package.json`),
-        `{
+    fs.outputFile(
+      path.resolve(outDir, `package.json`),
+      `{
           "name": "smarty-ui-vite/${name}",
           "main": "index.umd.js",
           "module": "index.umd.js"
 }`,
-        `utf-8`
-      );
-    });
+      `utf-8`
+    );
+  }
 };
 
 buildAll();
