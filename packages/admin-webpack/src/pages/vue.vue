cea313910b015ed56838c261d3c2a33b54b37baf
josephxia <15906475@qq.com>
2022-11-18 11:10:11 +0800

    feat: add echart sample

--- packages/admin-webpack/src/pages/vue.vue
+++ packages/admin-webpack/src/pages/vue.vue
@@ -1,14 +1,41 @@
 <template>
-  <div>Vue</div>
-</template>
-<!-- <script lang="ts">
-export default {
-  menu: 'Vue全家桶',
-  icon: 'i-logos-vue'
-}
-</script>
+  <div>Vue3</div>
+  <!-- <div id="c1" s="200px"></div> -->
+  <div id="c1" style="width: 600px;height:400px;"></div>
 
-<route lang="yaml">
-meta:
-  layout: default
-</route> -->
+
+</template>
+<script setup lang="ts">
+import * as echarts from 'echarts';
+import { onMounted } from 'vue'
+// 基于准备好的dom，初始化echarts实例
+
+onMounted(() => {
+  const dom = document.getElementById('c1')
+
+  // console.log('dom', dom)
+
+
+  var myChart = echarts.init(dom as HTMLElement);
+  // 绘制图表
+  myChart.setOption({
+    title: {
+      text: 'ECharts 入门示例'
+    },
+    tooltip: {},
+    xAxis: {
+      data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+    },
+    yAxis: {},
+    series: [
+      {
+        name: '销量',
+        type: 'bar',
+        data: [5, 20, 36, 10, 10, 20]
+      }
+    ]
+  });
+
+})
+
+</script>
