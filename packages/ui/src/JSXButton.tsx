6ccb197f26404a1483c6562b84cdd102acb7d675
hejunjie04 <hejunjie04@baidu.com>
2023-01-24 13:53:04 +0800

    feactor: 研究如何提pr

--- packages/ui/src/JSXButton.tsx
+++ packages/ui/src/JSXButton.tsx
@@ -2,7 +2,6 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "JSXButton",
-  // template:'<button>MyButton</button>'
   render() {
     return <button>JSX 666</button>;
   },
